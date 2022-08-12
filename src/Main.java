4e8923ad63122d38188ce0d5625e1c61221673a7
OmidHdr <omidnet42@yahoo.com>
2022-08-13 00:16:46 +0430

    initialise variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int[] number1 = {1,2,3,4,5,5,6,2,3};
+        int[] number2 = {4,2,8,15,18,6,3,8};
+        String[] str1 = {"one","two","three","four","five"};
+        String[] str2 = {"one","six","seven","two","nine","one"};
+        Scanner input = new Scanner(System.in);
+
         System.out.println("Merge two arrays");
     }
 }
