0f2896d430b6bd1dd92491bf8a8e954127ad7e1b
OmidHdr <omidnet42@yahoo.com>
2022-08-17 08:56:58 +0430

    version 2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,43 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+
 public class Main {
+
+    public static <T> ArrayList<T> removeDuplicates(ArrayList<T> list) {
+        ArrayList<T> newList = new ArrayList<T>();
+        for (T element : list) {
+            if (!newList.contains(element)) {
+                newList.add(element);
+            }
+        }
+        return newList;
+    }
+
+
     public static void main(String[] args) {
         int[] number1 = {1,2,3,4,5,5,6,2,3};
-        int[] number2 = {4,2,8,15,18,6,3,8};
-        String[] str1 = {"one","two","three","four","five"};
-        String[] str2 = {"one","six","seven","two","nine","one"};
-        Scanner input = new Scanner(System.in);
-        System.out.println("Merge two arrays");
-//        int[] maxNumber = number1+number2;
-        System.out.println("Hello world ! ");
-        System.out.println("Hello Mojtaba ! ");
-
+        int[] number2 = {4,2,8,15,18,6,3,8,5};
+//        String[] str1 = {"one","two","three","four","five"};
+//        String[] str2 = {"one","six","seven","two","nine","one"};
+        int[] result = new int[number1.length + number2.length];
+        for (int i = 0; i < number1.length; i++) {
+            result[i] = number1[i];
+            result[number1.length + i] = number2[i];
+        }
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int x = 0; x < result.length; x++) {
+            list.add(result[x]);
+        }
+        ArrayList<Integer> newList = removeDuplicates(list);
+        Collections.sort(newList);
+        for (int file : newList)
+            System.out.print(" " + file);
     }
 }
+
